838f9d944452b5d8f4d07016e463a749e666d53a
Salihah Alnahdi <salihah.alnahdi@kcl.ac.uk>
2022-10-08 19:45:13 +0100

    Frequency Solved

--- frequencies.py
+++ frequencies.py
@@ -1,14 +1,9 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 from collections import Counter
+import collections
 items = (['a','a','b','b','b','c'])
 def frequencies(items):
-    items = (['a','a','b','b','b','c'])
-    newitems = []
-    # Your code goes here
-    for i in items:
-        if i not in newitems:
-            newitems.append(i)
-            for j in range (len(newitems-1)):
-                str(newitems[j]).count(str(items[i])) 
-    return frequencies
+    newitems = collections.Counter(items)
+
+    return dict(frequencies)
