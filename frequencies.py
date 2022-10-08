88dfa0b97e86e859d1bb8d723c7eb410c37658a0
Salihah Alnahdi <salihah.alnahdi@kcl.ac.uk>
2022-10-08 19:57:07 +0100

    solved

--- frequencies.py
+++ frequencies.py
@@ -2,7 +2,7 @@
 """ENTER YOUR SOLUTION HERE!"""
 import collections
 def frequencies(items):
-    str(items)
+    newitems = [tuple(str(ele) for ele in sub) for sub in items]
     newitems = collections.Counter(items)
 
     return dict(newitems)
