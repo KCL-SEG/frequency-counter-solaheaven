d32757521124e7d30465b4d64b2953d37e9db756
Salihah Alnahdi <salihah.alnahdi@kcl.ac.uk>
2022-10-08 20:45:14 +0100

    solved

--- frequencies.py
+++ frequencies.py
@@ -6,15 +6,13 @@ def frequencies(items):
     if len(items) == 0:
         return {}
     else:
-         freq = {}
-    for item in str(items):
-        if (item in freq):
-            freq[item] += 1
-        else:
-            freq[item] = 1
- 
-    for key, value in freq.items():
-        print (key + ": " + value)
+        freq = {}
+        for item in str(items):
+            if (item in freq):
+                freq[item] += 1
+            else:
+                freq[item] = 1
+        return (freq)
     
 
    
