fa87a676cb66767eea46910917a39653bc43e7e1
Salihah Alnahdi <salihah.alnahdi@kcl.ac.uk>
2022-10-08 21:19:59 +0100

    solved

--- frequencies.py
+++ frequencies.py
@@ -5,13 +5,12 @@ from collections import Counter
 def frequencies(items):
     if len(items) == 0:
         return {}
-    elif items == [100, 'Hello', '100', '100', 100]:
-        return { '100': 4, 'Hello': 1 }
-
     else:
         freq = {}
         for item in str(items):
             if (item in freq):
+                freq[item] += 1
+            if (type(item) is int) and item in freq:
                 freq[item] += 1
             else:
                 freq[item] = 1
