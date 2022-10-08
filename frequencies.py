338cc547b660179d7efb80a2e32fa01511ebad94
Salihah Alnahdi <salihah.alnahdi@kcl.ac.uk>
2022-10-08 19:47:46 +0100

    Solved

--- frequencies.py
+++ frequencies.py
@@ -1,9 +1,7 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
-from collections import Counter
 import collections
-items = (['a','a','b','b','b','c'])
 def frequencies(items):
     newitems = collections.Counter(items)
 
-    return dict(frequencies)
+    return dict(newitems)
