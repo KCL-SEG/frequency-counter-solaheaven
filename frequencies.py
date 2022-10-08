895642298eb408df207ca28b3e7aaffcc3de1fd8
Salihah Alnahdi <salihah.alnahdi@kcl.ac.uk>
2022-10-08 20:29:05 +0100

    s

--- frequencies.py
+++ frequencies.py
@@ -3,10 +3,15 @@
 
 from collections import Counter
 def frequencies(items):
-    items = [str(i) for i in items]
-    freq = dict(Counter(i for sub in items for i in set(sub)))
-    for key in (freq.keys()):
-         print(key + ': ' + str(freq[key]))
+    if len(items) == 0:
+        print('{}')
+    else:
+        items = [str(i) for i in items]
+        freq = dict(Counter(i for sub in items for i in (sub)))
+        for key in (freq.keys()):
+             print(key + ': ' + str(freq[key]))
+
+    
 
 
 
